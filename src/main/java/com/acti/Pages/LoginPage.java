96883e6f582df3e017e49f127272b4fe601d1d08
neetu <neetu@192.168.86.46>
2020-05-30 20:05:42 -0700

    Created Login Page 05/30/2020 Neetu

--- src/main/java/com/acti/Pages/LoginPage.java
+++ src/main/java/com/acti/Pages/LoginPage.java
@@ -1,5 +1,50 @@
 package com.acti.Pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-}
+import com.actiBase.DriverScript;
+
+public class LoginPage extends DriverScript {
+	
+	//*****************page elements88*********//
+	
+	@FindBy(id="logoContainer") WebElement actiLogo;
+	@FindBy(id="username") WebElement UsernameTB;
+	@FindBy(name="pwd") WebElement PasswordTB;
+	@FindBy(id ="loginButton") WebElement loginButton;
+	@FindBy(id="KeepLogedInCheckBox") WebElement Loggedincheckbox;
+	@FindBy(id="toPasswordRecoveryPageLink") WebElement forgotpasswordlink;
+	
+	//********** page initialization************//
+	
+	public LoginPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+   //****************page action or methods***********//
+	
+	public boolean verifyActiLogo()
+	{
+		return actiLogo.isDisplayed();
+	}
+	public void ValidateLogin(String username, String password)
+	{
+		UsernameTB.sendKeys(username);
+		PasswordTB.sendKeys(password);
+		Loggedincheckbox.click();
+		loginButton.click();
+	}
+	
+	public String VerifyPageTitle()
+	{
+           return driver.getTitle();
+	}
+	public boolean VerifyLoggedInBox()
+	{
+	 return Loggedincheckbox.isDisplayed();
+	}
+	}
+
